c4efa29296d41af29edb120e0ed56e9a803c4456
Huy Lai <lai.huy@tamu.edu>
2022-10-27 12:06:55 -0500

    synch

---
+++ Lab 5/lab5_prob6.c
@@ -0,0 +1,14 @@
+//File named "lab5_prob6.c"
+#include <stdio.h>
+int very_fast_function(int i){
+	if ( (i*64 +1) > 1024) return i++;
+	else return 0;
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	i=40;
+printf("The function value of  i is %d\n", very_fast_function(i) );
+	return 0;
+}
