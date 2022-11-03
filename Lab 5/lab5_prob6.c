4157abdb4103703ada48e7ad43acac4f31212a76
Huy Lai <lai.huy@tamu.edu>
2022-11-03 08:58:56 -0500

    synch

--- Lab 5/lab5_prob6.c
+++ Lab 5/lab5_prob6.c
@@ -1,14 +1,33 @@
-//File named "lab5_prob6.c"
+// File named "lab5_prob6.c"
 #include <stdio.h>
-int very_fast_function(int i){
-	if ( (i*64 +1) > 1024) return i++;
-	else return 0;
+int very_fast_function(int i)
+{
+	if ((i * 64 + 1) > 1024)
+		return i++;
+	else
+		return 0;
+	__asm__(
+		"rrmovq %rbx, %rcx"
+		"rraddq %rcx, %rcx\n\t"
+		"rraddq %rcx, %rcx\n\t"
+		"rraddq %rcx, %rcx\n\t"
+		"rraddq %rcx, %rcx\n\t"
+		"rraddq %rcx, %rcx\n\t"
+		"iraddq $0x1, %rcx\n\t"
+		"isubq $0x400, %rcx\n\t"
+		"jle else\n\t"
+		"iraddq $0x1, %rbx\n\t"
+		"rrmovq %rbx, %rax\n\t"
+		"ret\n\t"
+		"else:\n\t"
+		"irmovq $0x0, %rax\n\t"
+		"ret\n\t");
 }
 
 int main(int argc, char *argv[])
 {
 	int i;
-	i=40;
-printf("The function value of  i is %d\n", very_fast_function(i) );
+	i = 40;
+	printf("The function value of  i is %d\n", very_fast_function(i));
 	return 0;
 }
